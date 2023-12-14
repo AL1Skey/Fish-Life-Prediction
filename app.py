53ffbe14a243279639051d963e1d012d2a88f4a5
AL1Skey <adamnurramadan@gmail.com>
2023-12-14 07:53:22 +0700

    commit

--- app.py
+++ app.py
@@ -13,16 +13,30 @@ feature = json.load(open('feature.json','r'))
 
 # Title
 st.header("Fish Life Prediction")
-
+st.image('fish.jpg')
 #DataFrame
 st.header("DataFrame")
-df.head()
+st.write(df.head())
 
 def user_input():
     inputter={}
     for i in feature:
-        inputter[i] = st.sidebar.slider(i,df.i.min(),df.i.max())
+        inputter[i] = st.sidebar.slider(i,df[i].min(),df[i].max())
     inputter = pd.DataFrame([inputter])
     return inputter
 
 input = user_input()
+
+#Input Section
+st.subheader("Input")
+st.write(input)
+
+if st.button('predict'):
+    prediction = model.predict(input)
+    
+    if prediction == 1:
+        prediction = 'Fish Can life in this water environment'
+    else:
+        prediction = 'Fish Can\'t life in this water environment'
+    
+    st.header(prediction)
